ff6e27033cba2122c2880c22f25981a17c6e58b8
Shugo Kawamura <bitoreieru@gmail.com>
2023-11-22 12:48:21 -0500

    CS2向け改修 (#1)
    * CS2向け改修
    
    * CS2のHTTPログの正規表現prefixを追加
    
    * メインのロジックを構造体に閉じ込めた
    
    * Added comments
    
    * Handler will not abort anymore

---
+++ cs2log_http.go
@@ -0,0 +1,56 @@
+package cs2loghttp
+
+import (
+	"bufio"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	cs2log "github.com/janstuemmel/cs2-log"
+)
+
+// httpLogPattern is a regexp to parse the log line on HTTP
+var httpLogPattern = regexp.MustCompile(`(\d{2}\/\d{2}\/\d{4} - \d{2}:\d{2}:\d{2}.\d{3}) - (.*)`)
+
+// NewLogHandler returns a new LogHandler. This function has side effect to override log line prefix.
+func NewLogHandler(h func(cs2log.Message)) LogHandler {
+	// Override log line prefix
+	cs2log.LogLinePattern = httpLogPattern
+	return LogHandler{
+		handler: h,
+	}
+}
+
+// LogHandler is a handler for cs2-log HTTP.
+type LogHandler struct {
+	handler func(cs2log.Message)
+}
+
+// Handle returns a gin.HandlerFunc to handle cs2-log HTTP.
+func (l *LogHandler) Handle() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		raw, err := c.GetRawData()
+		if err != nil {
+			log.Printf("Failed to get raw data : %v\n", err)
+			c.String(http.StatusInternalServerError, err.Error())
+			c.Abort()
+			return
+		}
+
+		sl := strings.NewReader(string(raw))
+		scanner := bufio.NewScanner(sl)
+		for scanner.Scan() {
+			msg, err := cs2log.Parse(scanner.Text())
+			if err != nil {
+				log.Printf("Failed to parse data : %v\n", err)
+				c.String(http.StatusInternalServerError, err.Error())
+				c.Abort()
+				return
+			}
+			l.handler(msg)
+		}
+		c.String(http.StatusOK, "OK")
+	}
+}
--- example/main.go
+++ example/main.go
@@ -1,58 +1,45 @@
 package main
 
 import (
-	"github.com/FlowingSPDG/csgo-log"
-	"github.com/FlowingSPDG/csgo-log-http"
-	"github.com/gin-gonic/gin"
 	"log"
+
+	cs2loghttp "github.com/FlowingSPDG/cs2-log-http"
+	"github.com/gin-gonic/gin"
+	cs2log "github.com/janstuemmel/cs2-log"
 )
-
-// File log format
-// L 11/12/2018 - 19:57:28: World triggered "Round_Start"
-// csgolog.LogLinePattern = regexp.MustCompile(`L (\d{2}\/\d{2}\/\d{4} - \d{2}:\d{2}:\d{2}): (.*)`)
-
-// HTTP log format
-// 02/09/2020 - 22:42:32.000 - World triggered "Round_Start"
-// csgolog.LogLinePattern = regexp.MustCompile(`(\d{2}\/\d{2}\/\d{4} - \d{2}:\d{2}:\d{2}.\d{3}) - (.*)`)
 
 func main() {
 	r := gin.Default()
-	r.POST("/csgolog", csgologhttp.CSGOLogger(MessageHandler))
+	logHandler := cs2loghttp.NewLogHandler(messageHandler)
+	r.POST("/csgolog", logHandler.Handle())
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Hello!"})
 	})
 	log.Panicf("Failed to listen port 3090 : %v\n", r.Run("0.0.0.0:3090"))
 }
 
-// MessageHandler handles message from CSGO Server and Gin middleware
-func MessageHandler(msg csgolog.Message, c *gin.Context) {
+func messageHandler(msg cs2log.Message) {
 	switch m := msg.(type) {
-	case csgolog.PlayerEntered:
+	case cs2log.PlayerEntered:
 		log.Printf("PlayerEntered : %v\n", m)
-	case csgolog.PlayerConnected:
+	case cs2log.PlayerConnected:
 		log.Printf("PlayerConnected : %v\n", m)
-	case csgolog.WorldMatchStart:
+	case cs2log.WorldMatchStart:
 		log.Printf("WorldMatchStart : %v\n", m)
-	case csgolog.TeamScored:
+	case cs2log.TeamScored:
 		log.Printf("TeamScored : %v\n", m)
-	case csgolog.GameOver:
+	case cs2log.GameOver:
 		log.Printf("GameOver : %v\n", m)
-	case csgolog.PlayerAttack:
+	case cs2log.PlayerAttack:
 		log.Printf("PlayerAttack : %v\n", m)
-	case csgolog.PlayerKill:
+	case cs2log.PlayerKill:
 		log.Printf("PlayerKill : %v\n", m)
 		log.Printf("Meta : %v\n", m.Meta)
-	case csgolog.PlayerPurchase:
+	case cs2log.PlayerPurchase:
 		log.Printf("PlayerPurchase : %v\n", m)
-	case csgolog.PlayerSay:
+	case cs2log.PlayerSay:
 		log.Printf("PlayerSay : %v\n", m)
-	case csgolog.ServerCvar:
-		log.Printf("ServerCvar : %v\n", m)
-	case csgolog.Get5Event:
-		log.Printf("Get5Event : [%v]\n", m)
-	case csgolog.PlayerKillOther:
-		log.Printf("PlayerKillOther : [%v]\n", m)
-	case csgolog.Unknown:
+	case cs2log.Unknown:
 		log.Printf("Unknown : [%v]\n", m.Raw)
 
 	default:
